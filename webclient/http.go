0863cfd740ac305078f967fff85bec777949782f
vorishirne <vorishirne@gmail.com>
2021-10-12 14:32:25 +0530

    webclient timeout

--- webclient/http.go
+++ webclient/http.go
@@ -10,12 +10,12 @@ import (
 func HTTPResponse(url string) (data *[]byte, err error) {
 	var netTransport = &http.Transport{
 		DialContext: (&net.Dialer{
-			Timeout: 5 * time.Second,
+			Timeout: 6 * time.Second,
 		}).DialContext,
-		TLSHandshakeTimeout: 5 * time.Second,
+		TLSHandshakeTimeout: 6 * time.Second,
 	}
 	var netClient = &http.Client{
-		Timeout:   time.Second * 10,
+		Timeout:   time.Second * 6,
 		Transport: netTransport,
 	}
 	response, _ := netClient.Get(url)
